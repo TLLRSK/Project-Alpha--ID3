762dd4765f52f368ea71b479aa7fb5e3c9462e75
TLLRSK <112385550+TLLRSK@users.noreply.github.com>
2022-12-20 16:37:49 +0100

    added portrait, added link navigation

--- script.js
+++ script.js
@@ -22,18 +22,41 @@ lightToggle.addEventListener('click', () => {
 });
 
 // MENU
+/* Opening & closing menu */
 const menuToggle = document.querySelector('#menu-btn__toggle');
 const menuBtn = document.querySelector('.nav__btn--menu').style;
 const arrowBtn = document.querySelector('.nav__btn--arrow').style;
 
-menuToggle.addEventListener('click', () => {
+
+menuToggle.addEventListener('click', (event) => {
     if (menuToggle.checked) {
-        console.log('menu is open');
         menuBtn.right = '3rem';
         arrowBtn.bottom = '-4rem';
     } else {
-        console.log('menu is not open');
         menuBtn.right = '5rem';
         arrowBtn.bottom = '4rem';
+    }
+})
+
+/* Using menu links */
+const menu = document.querySelector('#menu');
+
+menu.addEventListener('click', (event) => {
+    if (event.target.classList.contains('menu-link')) {
+        menuToggle.checked = false;
+        menuBtn.right = '5rem';
+        arrowBtn.bottom = '4rem';
+    };
+})
+
+/* Portrait toggle */
+const portraitBox = document.querySelector('.home__portrait-box');
+const portraitToggle = document.querySelector('#portrait-toggle');
+
+portraitBox.addEventListener('click', (event) => {
+    if (portraitToggle.checked) {
+        portraitBox.classList.add('open')
+    } else {
+        portraitBox.classList.remove('open')
     }
 })
